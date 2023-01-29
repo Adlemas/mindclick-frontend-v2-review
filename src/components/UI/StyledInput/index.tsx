6eebf9035d052c1ae4c044ceb9a7a7c3f55fa3ee
Adlemas <qwer.009771@gmail.com>
2023-01-29 22:17:08 +0100

    feat: type = password support for styled input

--- src/components/UI/StyledInput/index.tsx
+++ src/components/UI/StyledInput/index.tsx
@@ -3,6 +3,14 @@ import { Input, InputProps } from "antd";
 
 interface IStyledInputProps extends InputProps {}
 
-const StyledInput: FC<IStyledInputProps> = (props) => <Input {...props} />;
+const StyledInput: FC<IStyledInputProps> = (props) => {
+  const { type, ...rest } = props;
+
+  if (type === "password") {
+    return <Input.Password {...rest} />;
+  }
+
+  return <Input {...rest} />;
+};
 
 export default StyledInput;
