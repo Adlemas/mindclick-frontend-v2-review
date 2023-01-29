94f027cb69f00bf607f7012f41a89c3a3caa326a
Adlemas <qwer.009771@gmail.com>
2023-01-29 21:28:42 +0100

    feat&styles: provided basic login view styles

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -3,14 +3,20 @@ import { Card, Form, Typography } from "antd";
 import StyledInput from "@/components/UI/StyledInput";
 import Button from "@/components/UI/Button";
 
+import styles from "./styles.module.scss";
+
 const { Title } = Typography;
 const { Item } = Form;
 
 const LoginView: FC = () => (
-  <div>
-    <Card>
-      <Title level={2}>Добро пожаловать!</Title>
-      <Title level={4}>Пожалуйста, войдите в свой аккаунт</Title>
+  <div className={styles.loginView__Wrapper}>
+    <Card className={styles.loginView__Card}>
+      <Title level={2} className={styles.loginView__Title}>
+        Добро пожаловать!
+      </Title>
+      <Title type="secondary" level={4} className={styles.loginView__Subtitle}>
+        Пожалуйста, войдите в свой аккаунт
+      </Title>
       <Form layout="vertical">
         <Item label="Email" name="email">
           <StyledInput placeholder="Введите email" />
@@ -19,7 +25,7 @@ const LoginView: FC = () => (
           <StyledInput placeholder="Введите пароль" />
         </Item>
         <Item>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" block>
             Войти
           </Button>
         </Item>
