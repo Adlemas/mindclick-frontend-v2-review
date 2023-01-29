a7c42131c630e16fdde30124000d308254b21456
Adlemas <qwer.009771@gmail.com>
2023-01-29 22:32:36 +0100

    feat: dark theme support for outlined button

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,5 +1,6 @@
 import { Button as AntButton, ButtonProps, theme } from "antd";
 import { FC } from "react";
+import Theme from "@/types/theme";
 
 interface IButtonProps extends Omit<ButtonProps, "type"> {
   secondary?: boolean;
@@ -8,7 +9,7 @@ interface IButtonProps extends Omit<ButtonProps, "type"> {
 
 const Button: FC<IButtonProps> = (props) => {
   const { secondary, type, ...rest } = props;
-  const { token } = theme.useToken();
+  const { token, theme: currentTheme } = theme.useToken();
 
   const textColor = secondary ? token.colorWhite : token.colorTextLightSolid;
   const backgroundColor = secondary
@@ -26,8 +27,14 @@ const Button: FC<IButtonProps> = (props) => {
           backgroundColor,
         }),
         ...(type === "outline" && {
-          color: backgroundColor,
-          borderColor: backgroundColor,
+          color:
+            currentTheme.id === Theme.Dark
+              ? token.colorPrimary
+              : backgroundColor,
+          borderColor:
+            currentTheme.id === Theme.Dark
+              ? token.colorPrimary
+              : backgroundColor,
         }),
       }}
     />
---
+++ src/types/theme.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-shadow,no-unused-vars
+enum Theme {
+  Light = 0,
+  Dark = 1,
+  Compact = 2,
+}
+
+export default Theme;
