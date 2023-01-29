8cf1a55b9ba37cde195e61c10cea9d55045f913a
Adlemas <qwer.009771@gmail.com>
2023-01-29 21:50:16 +0100

    feat: secondary style support for UI button

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,8 +1,30 @@
-import { Button as AntButton, ButtonProps } from "antd";
+import { Button as AntButton, ButtonProps, theme } from "antd";
 import { FC } from "react";
 
-interface IButtonProps extends ButtonProps {}
+interface IButtonProps extends ButtonProps {
+  secondary?: boolean;
+}
 
-const Button: FC<IButtonProps> = (props) => <AntButton {...props} />;
+const Button: FC<IButtonProps> = (props) => {
+  const { secondary, ...rest } = props;
+  const { token } = theme.useToken();
+
+  return (
+    <AntButton
+      {...props}
+      style={{
+        ...rest.style,
+        backgroundColor: secondary
+          ? token.colorTextLightSolid
+          : token.colorPrimary,
+        color: secondary ? token.colorWhite : token.colorTextLightSolid,
+      }}
+    />
+  );
+};
+
+Button.defaultProps = {
+  secondary: false,
+};
 
 export default Button;
