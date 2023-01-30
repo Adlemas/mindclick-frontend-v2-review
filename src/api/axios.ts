0ba6545c15cc2cb690236d225664feab20bd060e
AmirAdal <amir.adal@cobold.in>
2023-01-30 06:55:51 +0100

    feat: provide accept language header in axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import Axios from "axios";
 const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
-    "Content-Type": "application/json",
+    "Accept-Language": "ru",
   },
 });
 
