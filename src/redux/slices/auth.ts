8cb0319c68df1cee943bfe561574c555ddc27296
AmirAdal <amir.adal@cobold.in>
2023-01-30 06:45:42 +0100

    feat: login action provided in auth slice

--- src/redux/slices/auth.ts
+++ src/redux/slices/auth.ts
@@ -1,5 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { AuthState } from "@/types/redux";
+import { LoginPayload, LoginResponse } from "@/types/api/auth";
+import login from "@/api/auth/login";
+import handleAxiosError from "@/utils/handleAxiosError";
 
 const initialState: AuthState = {
   accessToken: null,
@@ -8,11 +12,37 @@ const initialState: AuthState = {
   loading: false,
 };
 
+export const loginAction = createAsyncThunk<LoginResponse, LoginPayload>(
+  "auth/loginAction",
+  async (payload, { rejectWithValue }) => {
+    try {
+      return await login(payload);
+    } catch (err) {
+      handleAxiosError(err);
+      return rejectWithValue(err);
+    }
+  }
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     resetAuthState: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loginAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(loginAction.fulfilled, (state, action) => {
+      state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
+      state.isAuthenticated = true;
+      state.loading = false;
+    });
+    builder.addCase(loginAction.rejected, (state) => {
+      state.loading = false;
+    });
   },
 });
 
