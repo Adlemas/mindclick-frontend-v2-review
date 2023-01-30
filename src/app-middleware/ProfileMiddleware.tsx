7d0a26bac9fbb9ecaf2ad0e50701ad6c89dd1702
AmirAdal <amir.adal@cobold.in>
2023-01-30 21:39:04 +0100

    feat: provided profile middleware for getting profile information before loading page

--- src/app-middleware/ProfileMiddleware.tsx
+++ src/app-middleware/ProfileMiddleware.tsx
@@ -1,9 +1,44 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Spin } from "antd";
+import { isEqual } from "lodash";
 import { MiddlewareProps } from "@/types/middleware";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { getProfileAction } from "@/redux/slices/profile";
 
-const ProfileMiddleware: FC<MiddlewareProps> = ({ children }) => (
-  <Spin tip="Profile">{children}</Spin>
-);
+const ProfileMiddleware: FC<MiddlewareProps> = ({ children }) => {
+  const isAuthenticated = useAppSelector(
+    (state) => state.auth.isAuthenticated,
+    isEqual
+  );
+  const { loading } = useAppSelector((state) => state.profile, isEqual);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (isAuthenticated && !loading) {
+      dispatch(getProfileAction());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Spin tip="Загружаем данные профиля..." />
+      </div>
+    );
+  }
+
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>{children}</>
+  );
+};
 
 export default ProfileMiddleware;
