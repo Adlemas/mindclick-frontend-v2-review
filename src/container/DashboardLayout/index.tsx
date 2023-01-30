247cc3a6c3ee1dc46361d1de4c1982f52c7311b1
AmirAdal <amir.adal@cobold.in>
2023-01-30 22:10:30 +0100

    feat: dashboard layout init

---
+++ src/container/DashboardLayout/index.tsx
@@ -0,0 +1,26 @@
+import { FC, ReactNode } from "react";
+import { Image, Layout } from "antd";
+
+import LogoImg from "@/assets/mindclickon-icon.png";
+import DashboardMenu from "@/components/DashboardMenu";
+
+import styles from "./styles.module.scss";
+
+interface IDashboardLayoutProps {
+  children: ReactNode;
+}
+
+const { Sider, Content } = Layout;
+
+const DashboardLayout: FC<IDashboardLayoutProps> = ({ children }) => (
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  <Layout>
+    <Sider className={styles.sider}>
+      <Image src={LogoImg.src} alt="Logo" width={50} preview={false} />
+      <DashboardMenu />
+    </Sider>
+    <Content>{children}</Content>
+  </Layout>
+);
+
+export default DashboardLayout;
