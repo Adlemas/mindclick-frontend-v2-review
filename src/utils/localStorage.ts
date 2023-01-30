739ab6d2abeeaaed22f968ba5988008e2b9e8264
AmirAdal <amir.adal@cobold.in>
2023-01-30 07:06:41 +0100

    feat: localStorage utility

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef,no-console */
+export const setItemInLocal = (key: string, value: any) => {
+  if (!globalThis.window) {
+    return;
+  }
+  try {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getItemFromLocal = <T>(key: string): T | null => {
+  if (!globalThis.window) {
+    return null;
+  }
+  try {
+    const value = window.localStorage.getItem(key);
+    if (!value) {
+      return null;
+    }
+    return JSON.parse(value) as T;
+  } catch (error) {
+    console.error(error);
+  }
+  return null;
+};
+
+export const clearAllLocal = () => {
+  if (!globalThis.window) {
+    return;
+  }
+  try {
+    window.localStorage.clear();
+  } catch (error) {
+    console.error(error);
+  }
+};
