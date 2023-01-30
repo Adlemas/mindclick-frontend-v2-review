35f2bfc63b3e48be4ed418baa7a4b24bc2bba602
AmirAdal <amir.adal@cobold.in>
2023-01-30 22:46:41 +0100

    feat: started dashboard menu

--- src/components/DashboardMenu/index.tsx
+++ src/components/DashboardMenu/index.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
-import { RiLineChartLine } from "react-icons/ri";
+import { RiLineChartLine, RiUserLine } from "react-icons/ri";
+
+import styles from "./styles.module.scss";
 
 const { Item } = Menu;
 
 const DashboardMenu: FC = () => (
   <div>
-    <Menu theme="dark">
+    <Menu theme="light" className={styles.menu}>
       <Item>
         <Link href="/">
           <RiLineChartLine size={16} />
+        </Link>
+      </Item>
+      <Item>
+        <Link href="/members">
+          <RiUserLine size={16} />
         </Link>
       </Item>
     </Menu>
