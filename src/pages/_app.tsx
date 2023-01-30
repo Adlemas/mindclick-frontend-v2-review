1d70f9a6261c62c3d8ac139daa23dbaab7288870
AmirAdal <amir.adal@cobold.in>
2023-01-30 19:44:22 +0100

    feat: created app login middleware with refreshing functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,63 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import ruRU from "antd/locale/ru_RU";
-import { ConfigProvider, theme } from "antd";
+import { ConfigProvider, Spin, theme } from "antd";
 import { Provider } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { store } from "@/redux/store";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { getItemFromLocal } from "@/utils/localStorage";
+import { refreshAction } from "@/redux/slices/auth";
+
+interface MiddlewareProps {
+  Component: AppProps["Component"];
+  pageProps: AppProps["pageProps"];
+}
+
+const AppLoginMiddleware = ({ Component, pageProps }: MiddlewareProps) => {
+  const router = useRouter();
+  const { pathname } = router;
+
+  const dispatch = useAppDispatch();
+
+  const isAuthPage = pathname === "/login";
+  const { isAuthenticated, refreshing } = useAppSelector((state) => state.auth);
+  const localIsAuthenticated = getItemFromLocal("isAuthenticated");
+
+  useEffect(() => {
+    if (isAuthPage && isAuthenticated) {
+      router.push("/");
+      return;
+    }
+
+    if (!isAuthPage && !isAuthenticated) {
+      router.push("/login");
+      return;
+    }
+
+    if (!isAuthenticated && localIsAuthenticated) {
+      dispatch(refreshAction());
+    }
+  }, [localIsAuthenticated, isAuthenticated, isAuthPage, router, dispatch]);
+
+  if (refreshing || (isAuthPage && localIsAuthenticated)) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Spin tip="Загрузка..." />
+      </div>
+    );
+  }
+
+  return <Component {...pageProps} />;
+};
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider
@@ -30,7 +84,7 @@ const App = ({ Component, pageProps }: AppProps) => (
     }}
   >
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AppLoginMiddleware pageProps={pageProps} Component={Component} />
     </Provider>
   </ConfigProvider>
 );
