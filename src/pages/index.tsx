93d1f09235c036e971525eece068896d21cb8f20
AmirAdal <amir.adal@cobold.in>
2023-01-31 12:17:52 +0100

    feat: main user layout provided in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
 import type { NextPage } from "next";
+import DashboardNav from "@/components/DashboardNav";
+import Button from "@/components/UI/Button";
 
-const Home: NextPage = () => <div>Вот такой вот простой текст</div>;
+const Home: NextPage = () => (
+  <>
+    <DashboardNav>
+      <Button type="outline" secondary>
+        Выйти
+      </Button>
+    </DashboardNav>
+    <div>Вот такой вот простой текст</div>
+  </>
+);
 
 export default Home;
