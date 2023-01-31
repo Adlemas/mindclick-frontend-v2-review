3b3b06fd4bd3804ba343f9f0e57b7a8aec71d6bf
AmirAdal <amir.adal@cobold.in>
2023-01-31 21:18:53 +0100

    feat: provided add member functionality to open Panel

--- src/components/Members/MembersHeader/index.tsx
+++ src/components/Members/MembersHeader/index.tsx
@@ -1,12 +1,34 @@
 import type { FC } from "react";
 import { RiAddCircleLine } from "react-icons/ri";
+import { useState } from "react";
 import DashboardNav from "@/components/DashboardNav";
 import Button from "@/components/UI/Button";
+import Panel from "@/components/UI/Panel";
 
-const MembersHeader: FC = () => (
-  <DashboardNav title="Ученики">
-    <Button icon={<RiAddCircleLine />}>Добавить ученика</Button>
-  </DashboardNav>
-);
+const MembersHeader: FC = () => {
+  const [addMemberPanel, setAddMemberPanel] = useState<boolean>();
+
+  const handleAddMember = () => {
+    setAddMemberPanel(true);
+  };
+
+  return (
+    <>
+      <DashboardNav title="Ученики">
+        <Button icon={<RiAddCircleLine />} onClick={handleAddMember}>
+          Добавить ученика
+        </Button>
+      </DashboardNav>
+      <Panel
+        title="Новый ученик"
+        open={addMemberPanel}
+        width="25rem"
+        onClose={() => setAddMemberPanel(false)}
+      >
+        hello
+      </Panel>
+    </>
+  );
+};
 
 export default MembersHeader;
