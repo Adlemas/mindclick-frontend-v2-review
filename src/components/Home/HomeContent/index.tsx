875e3cc46bfba80cc3bb25e0a11d09a4d7b57e23
AmirAdal <amir.adal@cobold.in>
2023-01-31 15:54:36 +0100

    test: typography cross-theme support

--- src/components/Home/HomeContent/index.tsx
+++ src/components/Home/HomeContent/index.tsx
@@ -1,9 +1,12 @@
 import type { FC } from "react";
+import { Typography } from "antd";
 
 const HomeContent: FC = () => (
   <div>
-    <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+    <Typography.Title level={2}>Home</Typography.Title>
+    <Typography.Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+    </Typography.Paragraph>
   </div>
 );
 
