9ce6e206a787ff48c919956907e8d5377b8a50e4
AmirAdal <amir.adal@cobold.in>
2023-01-31 18:52:20 +0100

    feat: configuring table full height

--- src/components/Members/MembersTable/index.tsx
+++ src/components/Members/MembersTable/index.tsx
@@ -1,8 +1,32 @@
 import type { FC } from "react";
 import { Table } from "antd";
+import { ColumnsType } from "antd/lib/table";
+import { IUser } from "@/types/entity";
+
+import styles from "./styles.module.scss";
+
+const columns: ColumnsType<IUser> = [
+  {
+    title: "Имя",
+    dataIndex: "firstName",
+    key: "firstName",
+  },
+  {
+    title: "Фамилия",
+    dataIndex: "lastName",
+    key: "lastName",
+  },
+];
 
 const MembersTable: FC = () => (
-  <Table scroll={{ y: "calc(100vh - 72px - 65px)" }} />
+  <Table
+    className={styles.table}
+    columns={columns}
+    dataSource={[]}
+    scroll={{
+      y: "calc(100vh - 72px - 65px - 55px)",
+    }}
+  />
 );
 
 export default MembersTable;
