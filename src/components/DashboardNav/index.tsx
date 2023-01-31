e600ad372c74750ea7a514b51f4c4ac9c49ba14b
AmirAdal <amir.adal@cobold.in>
2023-01-31 12:20:24 +0100

    feat: title prop support that renders in the center

--- src/components/DashboardNav/index.tsx
+++ src/components/DashboardNav/index.tsx
@@ -7,18 +7,20 @@ const { Header } = Layout;
 
 interface IDashboardNavProps {
   children?: ReactNode;
+  title?: string;
 }
 
-const DashboardNav: FC<IDashboardNavProps> = ({ children }) => (
+const DashboardNav: FC<IDashboardNavProps> = ({ children, title }) => (
   <Header className={styles.header}>
     <div>left</div>
-    <div>middle</div>
+    <div>{title}</div>
     <div>{children}</div>
   </Header>
 );
 
 DashboardNav.defaultProps = {
   children: null,
+  title: "",
 };
 
 export default DashboardNav;
