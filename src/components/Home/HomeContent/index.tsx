45ec9fb9f61cf49d2be31931e0ac4c72ee1ccefc
AmirAdal <amir.adal@cobold.in>
2023-01-31 13:46:34 +0100

    feat: started HomeContent component

---
+++ src/components/Home/HomeContent/index.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+const HomeContent: FC = () => (
+  <div>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+  </div>
+);
+
+export default HomeContent;
