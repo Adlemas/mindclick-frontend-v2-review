c1fd231abda22dac2ff0b92f3941d67f929bf306
AmirAdal <amir.adal@cobold.in>
2023-01-31 13:45:11 +0100

    feat: provided logic item component for dashboard menu

--- src/components/DashboardMenu/index.tsx
+++ src/components/DashboardMenu/index.tsx
@@ -1,27 +1,92 @@
-import { FC } from "react";
-import { Menu } from "antd";
+import { FC, ReactNode, useState } from "react";
+import { Menu, theme } from "antd";
 import Link from "next/link";
-import { RiLineChartLine, RiUserLine } from "react-icons/ri";
+import { RiHomeFill, RiHomeLine, RiUserFill, RiUserLine } from "react-icons/ri";
 
+import { useRouter } from "next/router";
 import styles from "./styles.module.scss";
 
-const { Item } = Menu;
+interface ItemProps {
+  icon: ReactNode;
+  hoverIcon: ReactNode;
+  itemKey: string;
+  hoverKey?: string;
+  pathname: string;
+  // eslint-disable-next-line no-unused-vars
+  handleMouse: (key: string) => void;
+}
 
-const DashboardMenu: FC = () => (
-  <div>
-    <Menu theme="light" className={styles.menu}>
-      <Item>
-        <Link href="/">
-          <RiLineChartLine size={16} />
-        </Link>
-      </Item>
-      <Item>
-        <Link href="/members">
-          <RiUserLine size={16} />
-        </Link>
-      </Item>
-    </Menu>
-  </div>
-);
+const Item: FC<ItemProps> = (props) => {
+  const {
+    itemKey,
+    icon,
+    hoverIcon,
+    handleMouse,
+    hoverKey,
+    pathname,
+    ...nativeProps
+  } = props;
+
+  const { token } = theme.useToken();
+  const { colorPrimary, colorTextLightSolid } = token;
+
+  return (
+    <Menu.Item
+      {...nativeProps}
+      key={itemKey}
+      style={
+        pathname === itemKey
+          ? {
+              backgroundColor: colorPrimary,
+              color: colorTextLightSolid,
+            }
+          : {}
+      }
+      onMouseOver={() => handleMouse(itemKey)}
+      onMouseOut={() => handleMouse(itemKey)}
+    >
+      <Link href="/">
+        {pathname === itemKey || hoverKey === itemKey ? hoverIcon : icon}
+      </Link>
+    </Menu.Item>
+  );
+};
+
+Item.defaultProps = {
+  hoverKey: undefined,
+};
+
+const DashboardMenu: FC = () => {
+  const { pathname } = useRouter();
+
+  const selectedKeys = [pathname];
+  const [hoverKey, setHoverKey] = useState<string>();
+
+  const handleMouse = (key: string) =>
+    setHoverKey((prev) => (prev === key ? undefined : key));
+
+  return (
+    <div>
+      <Menu theme="light" className={styles.menu} selectedKeys={selectedKeys}>
+        <Item
+          icon={<RiHomeLine />}
+          hoverIcon={<RiHomeFill />}
+          itemKey="/"
+          hoverKey={hoverKey}
+          handleMouse={handleMouse}
+          pathname={pathname}
+        />
+        <Item
+          icon={<RiUserLine />}
+          hoverIcon={<RiUserFill />}
+          itemKey="/members"
+          hoverKey={hoverKey}
+          handleMouse={handleMouse}
+          pathname={pathname}
+        />
+      </Menu>
+    </div>
+  );
+};
 
 export default DashboardMenu;
