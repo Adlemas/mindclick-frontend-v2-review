a22c6633341d5f95362b419b1a3baa76dd48a6c8
AmirAdal <amir.adal@cobold.in>
2023-01-31 19:05:59 +0100

    test: provided data source for members table

--- src/components/Members/MembersTable/index.tsx
+++ src/components/Members/MembersTable/index.tsx
@@ -1,32 +1,55 @@
 import type { FC } from "react";
 import { Table } from "antd";
 import { ColumnsType } from "antd/lib/table";
+import { isEqual } from "lodash";
 import { IUser } from "@/types/entity";
 
 import styles from "./styles.module.scss";
+import getFullName from "@/utils/getFullName";
+import { useAppSelector } from "@/redux/hooks";
 
 const columns: ColumnsType<IUser> = [
   {
     title: "Имя",
     dataIndex: "firstName",
     key: "firstName",
+    render: (_, user) => getFullName(user),
   },
   {
-    title: "Фамилия",
-    dataIndex: "lastName",
-    key: "lastName",
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Очки",
+    dataIndex: "points",
+    key: "points",
+  },
+  {
+    title: "Кубков",
+    dataIndex: "rate",
+    key: "rate",
+  },
+  {
+    title: "Номер телефона",
+    dataIndex: "phone",
+    key: "phone",
   },
 ];
 
-const MembersTable: FC = () => (
-  <Table
-    className={styles.table}
-    columns={columns}
-    dataSource={[]}
-    scroll={{
-      y: "calc(100vh - 72px - 65px - 55px)",
-    }}
-  />
-);
+const MembersTable: FC = () => {
+  const { profile } = useAppSelector((state) => state.profile, isEqual);
+
+  return (
+    <Table
+      className={styles.table}
+      columns={columns}
+      dataSource={[profile]}
+      scroll={{
+        y: "calc(100vh - 72px - 65px - 55px)",
+      }}
+    />
+  );
+};
 
 export default MembersTable;
