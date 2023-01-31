d9dad80138847dbfbcc869ba01eb9518f77af97a
AmirAdal <amir.adal@cobold.in>
2023-01-31 21:04:08 +0100

    feat: provided type = "search" support in styled inpuot

--- src/components/UI/StyledInput/index.tsx
+++ src/components/UI/StyledInput/index.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 import { Input, InputProps } from "antd";
+import type { SearchProps } from "antd/lib/input";
 
-interface IStyledInputProps extends InputProps {}
+interface IStyledInputProps extends InputProps, SearchProps {}
 
 const StyledInput: FC<IStyledInputProps> = (props) => {
   const { type, ...rest } = props;
 
   if (type === "password") {
     return <Input.Password {...rest} />;
+  }
+
+  if (type === "search") {
+    return <Input.Search {...rest} />;
   }
 
   return <Input {...rest} />;
