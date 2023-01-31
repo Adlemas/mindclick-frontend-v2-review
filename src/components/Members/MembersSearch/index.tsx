8753f15abe6707281da3008c0c545e40cd9d5354
AmirAdal <amir.adal@cobold.in>
2023-01-31 16:50:39 +0100

    feat: created MembersSearch component for query param

---
+++ src/components/Members/MembersSearch/index.tsx
@@ -0,0 +1,31 @@
+import type { FC } from "react";
+import { Typography } from "antd";
+import { RiSearch2Line } from "react-icons/all";
+import StyledInput from "@/components/UI/StyledInput";
+import Button from "@/components/UI/Button";
+
+import styles from "./styles.module.scss";
+
+const { Text } = Typography;
+
+const MembersSearch: FC = () => (
+  <div className={styles.search__Wrapper}>
+    <div>
+      <StyledInput
+        className={styles.search__Input}
+        placeholder="Искать по имени/почте/группе"
+        allowClear
+      />
+      <Button icon={<RiSearch2Line />} className={styles.search__Button}>
+        Поиск
+      </Button>
+    </div>
+    <div>
+      <Text className={styles.search__Result}>
+        Найдено <Text strong>1200</Text> пользователей
+      </Text>
+    </div>
+  </div>
+);
+
+export default MembersSearch;
