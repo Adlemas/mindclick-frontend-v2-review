d3495ecc8ca0a45681c43dc1ab78d63374776146
AmirAdal <amir.adal@cobold.in>
2023-01-31 06:39:25 +0100

    feat: own children as right component for header support

--- src/components/DashboardNav/index.tsx
+++ src/components/DashboardNav/index.tsx
@@ -1,16 +1,24 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { Layout } from "antd";
 
 import styles from "./styles.module.scss";
 
 const { Header } = Layout;
 
-const DashboardNav: FC = () => (
+interface IDashboardNavProps {
+  children?: ReactNode;
+}
+
+const DashboardNav: FC<IDashboardNavProps> = ({ children }) => (
   <Header className={styles.header}>
     <div>left</div>
     <div>middle</div>
-    <div>right</div>
+    <div>{children}</div>
   </Header>
 );
+
+DashboardNav.defaultProps = {
+  children: null,
+};
 
 export default DashboardNav;
