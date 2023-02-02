9067fcd52682f4ca2dad2973f0f6bf26c5ec2acf
AmirAdal <amir.adal@cobold.in>
2023-02-02 10:48:34 +0100

    feat: dynamic cross-theme border for sider in dashboard layout

--- src/container/DashboardLayout/index.tsx
+++ src/container/DashboardLayout/index.tsx
@@ -1,10 +1,11 @@
 import { FC, ReactNode } from "react";
-import { Image, Layout } from "antd";
+import { Image, Layout, theme } from "antd";
 
 import LogoImg from "@/assets/mindclickon-icon.png";
 import DashboardMenu from "@/components/DashboardMenu";
 
 import styles from "./styles.module.scss";
+import Theme from "@/types/theme";
 
 interface IDashboardLayoutProps {
   children: ReactNode;
@@ -12,15 +13,25 @@ interface IDashboardLayoutProps {
 
 const { Sider, Content } = Layout;
 
-const DashboardLayout: FC<IDashboardLayoutProps> = ({ children }) => (
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  <Layout>
-    <Sider theme="light" className={styles.sider}>
-      <Image src={LogoImg.src} alt="Logo" width={50} preview={false} />
-      <DashboardMenu />
-    </Sider>
-    <Content className={styles.content}>{children}</Content>
-  </Layout>
-);
+const DashboardLayout: FC<IDashboardLayoutProps> = ({ children }) => {
+  const { theme: currentTheme } = theme.useToken();
+
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <Layout>
+      <Sider
+        theme="light"
+        className={styles.sider}
+        style={{
+          borderColor: currentTheme.id === Theme.Light ? "#e8e8e8" : "#252525",
+        }}
+      >
+        <Image src={LogoImg.src} alt="Logo" width={50} preview={false} />
+        <DashboardMenu />
+      </Sider>
+      <Content className={styles.content}>{children}</Content>
+    </Layout>
+  );
+};
 
 export default DashboardLayout;
