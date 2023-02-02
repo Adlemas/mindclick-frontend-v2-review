3509f57c2bf6909e2aaabec7174531fa5ae8c7e2
AmirAdal <amir.adal@cobold.in>
2023-02-02 15:19:56 +0100

    feat: moment date library webpack plugin installed and added in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const AntdMomentWebpackPlugin = require("@ant-design/moment-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.plugins = [...config.plugins, new AntdMomentWebpackPlugin()];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
