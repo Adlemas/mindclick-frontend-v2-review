93a7afbe7aafc5c87579f036806becf560179e80
Adlemas <qwer.009771@gmail.com>
2023-02-04 17:33:09 +0100

    feat: provided add group panel

--- src/components/Groups/GroupsHeader/index.tsx
+++ src/components/Groups/GroupsHeader/index.tsx
@@ -1,12 +1,38 @@
 import type { FC } from "react";
 import { RiAddCircleLine } from "react-icons/ri";
+import { useState } from "react";
 import DashboardNav from "@/components/DashboardNav";
 import Button from "@/components/UI/Button";
+import Panel from "@/components/UI/Panel";
 
-const GroupsHeader: FC = () => (
-  <DashboardNav title="Группы">
-    <Button icon={<RiAddCircleLine />}>Добавить группу</Button>
-  </DashboardNav>
-);
+const GroupsHeader: FC = () => {
+  const [addGroupPanel, setAddGroupPanel] = useState<boolean>();
+
+  const handleAddGroup = () => {
+    setAddGroupPanel(true);
+  };
+
+  const handleAddGroupCancel = () => {
+    setAddGroupPanel(false);
+  };
+
+  return (
+    <>
+      <DashboardNav title="Группы">
+        <Button icon={<RiAddCircleLine />} onClick={handleAddGroup}>
+          Добавить группу
+        </Button>
+      </DashboardNav>
+      <Panel
+        title="Новая группа"
+        open={addGroupPanel}
+        width="25rem"
+        onClose={handleAddGroupCancel}
+      >
+        <p>Форма добавления группы</p>
+      </Panel>
+    </>
+  );
+};
 
 export default GroupsHeader;
