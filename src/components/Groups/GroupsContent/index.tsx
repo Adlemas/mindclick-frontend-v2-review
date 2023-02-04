dfb254efd4439b9520df78f49e9bd3ec99d40130
Adlemas <qwer.009771@gmail.com>
2023-02-04 14:30:50 +0100

    fix: removed groups search as it is no need from groups content

--- src/components/Groups/GroupsContent/index.tsx
+++ src/components/Groups/GroupsContent/index.tsx
@@ -1,12 +1,6 @@
 import type { FC } from "react";
-import GroupsSearch from "@/components/Groups/GroupsSearch";
 import GroupsTable from "@/components/Groups/GroupsTable";
 
-const GroupsContent: FC = () => (
-  <>
-    <GroupsSearch />
-    <GroupsTable />
-  </>
-);
+const GroupsContent: FC = () => <GroupsTable />;
 
 export default GroupsContent;
