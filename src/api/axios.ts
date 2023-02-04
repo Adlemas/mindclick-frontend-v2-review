85caa40a3abcea31bd66cd28e5f1fbe2c33acc7d
Adlemas <qwer.009771@gmail.com>
2023-02-04 17:02:46 +0100

    feat: removed solution for multiple refreshing token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,6 @@
 import Axios from "axios";
 import { message } from "antd";
 import { getItemFromLocal, setItemInLocal } from "@/utils/localStorage";
-import { RefreshTokenResponse } from "@/types/api/auth";
 
 const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -23,52 +22,15 @@ const deleteSession = () => {
 
 const refreshTokens = async () => {
   const refreshToken = getItemFromLocal("refreshToken");
-  const refreshingToken = getItemFromLocal("refreshing.axios");
 
   if (refreshToken) {
-    if (refreshingToken) {
-      return new Promise<RefreshTokenResponse>((resolve, reject) => {
-        let timeSpent = 0;
-        const timeout = 10000;
-
-        const interval = setInterval(() => {
-          timeSpent += 100;
-          const stillRefreshing = getItemFromLocal("refreshing.axios");
-          if (!stillRefreshing) {
-            clearInterval(interval);
-            const accessToken = getItemFromLocal<string>("accessToken");
-            const refresh = getItemFromLocal<string>("refreshToken");
-            if (accessToken === null || refresh === null) {
-              return reject(
-                new Error("При обновлении токенов произошла ошибка")
-              );
-            }
-            return resolve({
-              accessToken,
-              refreshToken: refresh,
-            });
-          }
-          if (timeSpent >= timeout) {
-            clearInterval(interval);
-            return reject(
-              new Error("Время ожидания обновления токенов истекло")
-            );
-          }
-          return null;
-        }, 100);
-      });
-    }
-    setItemInLocal("refreshing.axios", true);
     const tokens = await axios.get("auth/refresh", {
       headers: {
         Authorization: `Bearer ${refreshToken}`,
       },
     });
-    setItemInLocal("refreshing.axios", false);
     return tokens.data;
   }
-
-  setItemInLocal("refreshing.axios", false);
 
   return null;
 };
