ec2b96b397cc35af870d4128fccf380685de693a
Adlemas <qwer.009771@gmail.com>
2023-02-04 13:46:20 +0100

    feat: provided the rest menu items in dashboard menu

--- src/components/DashboardMenu/index.tsx
+++ src/components/DashboardMenu/index.tsx
@@ -1,15 +1,33 @@
 import { FC, ReactNode, useState } from "react";
 import { Menu, theme } from "antd";
-import { RiHomeFill, RiHomeLine, RiUserFill, RiUserLine } from "react-icons/ri";
+import {
+  RiAdminFill,
+  RiAdminLine,
+  RiGamepadLine,
+  RiGroup2Fill,
+  RiGroup2Line,
+  RiHomeFill,
+  RiHomeLine,
+  RiPrinterLine,
+  RiSettings3Fill,
+  RiSettings3Line,
+  RiTaskFill,
+  RiTaskLine,
+  RiUserFill,
+  RiUserLine,
+  RiVideoChatFill,
+  RiVideoChatLine,
+} from "react-icons/ri";
 
 import { useRouter } from "next/router";
-import { MenuItemType } from "antd/lib/menu/hooks/useItems";
+import type { MenuItemType } from "antd/lib/menu/hooks/useItems";
 import styles from "./styles.module.scss";
 
-interface ItemProps {
-  icon: ReactNode;
-  hoverIcon: ReactNode;
-  itemKey: string;
+interface ItemProps extends Omit<MenuItemType, "key"> {
+  key: string;
+  icon?: ReactNode;
+  token: ReturnType<typeof theme.useToken>["token"];
+  hoverIcon?: ReactNode;
   hoverKey?: string;
   hoverTitle?: string;
   pathname: string;
@@ -21,30 +39,32 @@ interface ItemProps {
 
 const renderItem = (props: ItemProps): MenuItemType => {
   const {
-    itemKey,
+    key,
     icon,
     hoverIcon,
+    token,
     handleMouse,
     handleClick,
     hoverKey,
     hoverTitle,
     pathname,
+    ...rest
   } = props;
 
-  const { token } = theme.useToken();
   const { colorPrimary, colorTextLightSolid } = token;
 
   return {
-    key: itemKey,
-    icon: hoverKey === itemKey ? hoverIcon : icon,
+    key,
+    icon: hoverKey === key ? hoverIcon : icon,
     title: hoverTitle,
     style:
-      pathname === itemKey
+      pathname === key
         ? { color: colorTextLightSolid, backgroundColor: colorPrimary }
         : undefined,
-    onClick: () => handleClick(itemKey),
-    onMouseEnter: () => handleMouse(itemKey),
-    onMouseLeave: () => handleMouse(itemKey),
+    ...rest,
+    onClick: () => handleClick(key),
+    onMouseEnter: () => handleMouse(key),
+    onMouseLeave: () => handleMouse(key),
   };
 };
 
@@ -63,8 +83,11 @@ const DashboardMenu: FC = () => {
   const handleMouse = (key: string) =>
     setHoverKey((prev) => (prev === key ? undefined : key));
 
+  const { token } = theme.useToken();
+
   const commonProps = {
     pathname,
+    token,
     handleMouse,
     handleClick,
     hoverKey,
@@ -80,15 +103,119 @@ const DashboardMenu: FC = () => {
           renderItem({
             icon: <RiHomeLine />,
             hoverIcon: <RiHomeFill />,
-            itemKey: "/",
+            key: "/",
             hoverTitle: "Главная",
             ...commonProps,
           }),
           renderItem({
             icon: <RiUserLine />,
             hoverIcon: <RiUserFill />,
-            itemKey: "/members",
+            key: "/members",
             hoverTitle: "Участники",
+            ...commonProps,
+          }),
+          renderItem({
+            icon: <RiGroup2Line />,
+            hoverIcon: <RiGroup2Fill />,
+            key: "/groups",
+            hoverTitle: "Группы",
+            ...commonProps,
+          }),
+          {
+            icon: <RiGamepadLine />,
+            title: "Игры",
+            key: "/games",
+            children: [
+              renderItem({
+                key: "/games/mental-arithmetic",
+                hoverTitle: "Сложение/вычитание",
+                label: "Сложение/вычитание",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/games/multiply",
+                hoverTitle: "Умножение",
+                label: "Умножение",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/games/tables",
+                hoverTitle: "Столбики",
+                label: "Столбики",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/games/divide",
+                hoverTitle: "Деление",
+                label: "Деление",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/games/flash-cards",
+                hoverTitle: "Флеш-карты",
+                label: "Флеш-карты",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/games/shulte-table",
+                hoverTitle: "Таблица Шульте",
+                label: "Таблица Шульте",
+                ...commonProps,
+              }),
+            ],
+          },
+          {
+            icon: <RiPrinterLine />,
+            title: "Печать",
+            key: "/print",
+            children: [
+              renderItem({
+                key: "/print/mental-arithmetic",
+                hoverTitle: "Сложение/вычитание",
+                label: "Сложение/вычитание",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/print/multiply",
+                hoverTitle: "Умножение",
+                label: "Умножение",
+                ...commonProps,
+              }),
+              renderItem({
+                key: "/print/divide",
+                hoverTitle: "Деление",
+                label: "Деление",
+                ...commonProps,
+              }),
+            ],
+          },
+          renderItem({
+            icon: <RiVideoChatLine />,
+            hoverIcon: <RiVideoChatFill />,
+            key: "/video",
+            hoverTitle: "Конференции",
+            ...commonProps,
+          }),
+          renderItem({
+            icon: <RiTaskLine />,
+            hoverIcon: <RiTaskFill />,
+            key: "/tasks",
+            hoverTitle: "Задания",
+            ...commonProps,
+          }),
+          renderItem({
+            icon: <RiAdminLine />,
+            hoverIcon: <RiAdminFill />,
+            key: "/admin",
+            hoverTitle: "Администрирование",
+            ...commonProps,
+          }),
+          renderItem({
+            icon: <RiSettings3Line />,
+            hoverIcon: <RiSettings3Fill />,
+            key: "/settings",
+            hoverTitle: "Настройки",
+            className: styles.settings,
             ...commonProps,
           }),
         ]}
