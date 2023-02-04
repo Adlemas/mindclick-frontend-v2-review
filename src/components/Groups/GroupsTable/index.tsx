6a7a1f3f61ca8e7c2a0258179ce9445bf4dc9c93
Adlemas <qwer.009771@gmail.com>
2023-02-04 17:48:23 +0100

    feat: provided createdAt column in groups table

--- src/components/Groups/GroupsTable/index.tsx
+++ src/components/Groups/GroupsTable/index.tsx
@@ -4,6 +4,7 @@ import { ColumnsType } from "antd/lib/table";
 import { isEqual } from "lodash";
 import { useEffect } from "react";
 import { RiEditFill } from "react-icons/ri";
+import moment from "moment";
 import type { IGroup } from "@/types/entity";
 
 import styles from "./styles.module.scss";
@@ -27,6 +28,12 @@ const columns: ColumnsType<IGroup> = [
     dataIndex: "color",
     key: "color",
     render: (color) => <Tag color={color}>{color ?? "Нет цвета"}</Tag>,
+  },
+  {
+    title: "Создана",
+    dataIndex: "createdAt",
+    key: "createdAt",
+    render: (createdAt: string) => moment(createdAt).format("DD.MM.YYYY HH:mm"),
   },
   {
     title: "Действия",
