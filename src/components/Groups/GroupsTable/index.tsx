522c1758ea7bb2ad3546f00cf96f747edfcd368b
Adlemas <qwer.009771@gmail.com>
2023-02-05 15:13:27 +0100

    feat: integrated update group panel & slice in groups table

--- src/components/Groups/GroupsTable/index.tsx
+++ src/components/Groups/GroupsTable/index.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { Table, Tag } from "antd";
 import { ColumnsType } from "antd/lib/table";
 import { isEqual } from "lodash";
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { RiEditFill } from "react-icons/ri";
 import moment from "moment";
 import type { IGroup } from "@/types/entity";
@@ -10,46 +10,72 @@ import type { IGroup } from "@/types/entity";
 import styles from "./styles.module.scss";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import Button from "@/components/UI/Button";
-import { getGroupsAction } from "@/redux/slices/groups";
-
-const columns: ColumnsType<IGroup> = [
-  {
-    title: "Название",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Максимальное количество участников",
-    dataIndex: "maxMembers",
-    key: "maxMembers",
-  },
-  {
-    title: "Цвет",
-    dataIndex: "color",
-    key: "color",
-    render: (color) => <Tag color={color}>{color ?? "Нет цвета"}</Tag>,
-  },
-  {
-    title: "Создана",
-    dataIndex: "createdAt",
-    key: "createdAt",
-    render: (createdAt: string) => moment(createdAt).format("DD.MM.YYYY HH:mm"),
-  },
-  {
-    title: "Действия",
-    dataIndex: "actions",
-    key: "actions",
-    render: () => (
-      <Button icon={<RiEditFill />} type="outline" secondary>
-        Редактировать
-      </Button>
-    ),
-  },
-];
+import { getGroupsAction, setGroup } from "@/redux/slices/groups";
+import Panel from "@/components/UI/Panel";
+import AddGroupForm from "@/forms/AddGroupForm";
 
 const GroupsTable: FC = () => {
   const { loading, records } = useAppSelector((state) => state.groups, isEqual);
   const dispatch = useAppDispatch();
+
+  const [updateGroupPanel, setUpdateGroupPanel] = useState<boolean>(false);
+
+  const handleUpdateGroup = useCallback(
+    (group: IGroup) => {
+      dispatch(setGroup(group));
+      setUpdateGroupPanel(true);
+    },
+    [dispatch]
+  );
+
+  const handleUpdateGroupCancel = () => {
+    dispatch(setGroup(null));
+    setUpdateGroupPanel(false);
+  };
+
+  const columns: ColumnsType<IGroup> = useMemo(
+    () => [
+      {
+        title: "Название",
+        dataIndex: "name",
+        key: "name",
+      },
+      {
+        title: "Максимальное количество участников",
+        dataIndex: "maxMembers",
+        key: "maxMembers",
+      },
+      {
+        title: "Цвет",
+        dataIndex: "color",
+        key: "color",
+        render: (color) => <Tag color={color}>{color ?? "Нет цвета"}</Tag>,
+      },
+      {
+        title: "Создана",
+        dataIndex: "createdAt",
+        key: "createdAt",
+        render: (createdAt: string) =>
+          moment(createdAt).format("DD.MM.YYYY HH:mm"),
+      },
+      {
+        title: "Действия",
+        dataIndex: "actions",
+        key: "actions",
+        render: (_, group) => (
+          <Button
+            icon={<RiEditFill />}
+            type="outline"
+            onClick={() => handleUpdateGroup(group)}
+            secondary
+          >
+            Редактировать
+          </Button>
+        ),
+      },
+    ],
+    []
+  );
 
   useEffect(() => {
     dispatch(getGroupsAction());
@@ -57,13 +83,23 @@ const GroupsTable: FC = () => {
   }, []);
 
   return (
-    <Table
-      className={styles.table}
-      columns={columns}
-      loading={loading}
-      dataSource={records}
-      pagination={false}
-    />
+    <>
+      <Table
+        className={styles.table}
+        columns={columns}
+        loading={loading}
+        dataSource={records}
+        pagination={false}
+      />
+      <Panel
+        title="Редактирование группы"
+        open={updateGroupPanel}
+        width="25rem"
+        onClose={handleUpdateGroupCancel}
+      >
+        <AddGroupForm />
+      </Panel>
+    </>
   );
 };
 
