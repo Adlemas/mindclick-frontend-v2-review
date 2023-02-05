163f505887e01e37127c062de3f1bac281150cae
Adlemas <qwer.009771@gmail.com>
2023-02-05 14:10:21 +0100

    feat: provided cross-theme styles for color picker

--- src/components/UI/ColorPicker/index.tsx
+++ src/components/UI/ColorPicker/index.tsx
@@ -1,9 +1,11 @@
 import type { FC } from "react";
 import { BlockPicker } from "react-color";
-import { Popover } from "antd";
+import { Popover, theme } from "antd";
+import classNames from "classnames";
 import StyledInput from "@/components/UI/StyledInput";
 
 import styles from "./styles.module.scss";
+import Theme from "@/types/theme";
 
 interface ColorPickerProps {
   color?: string;
@@ -11,31 +13,39 @@ interface ColorPickerProps {
   onChange?: (color: string) => void;
 }
 
-const ColorPicker: FC<ColorPickerProps> = ({ color, onChange }) => (
-  <StyledInput
-    value={color}
-    onChange={(e) => {
-      if (onChange) onChange(e.target.value);
-    }}
-    placeholder="Например #345f42"
-    addonBefore={
-      <Popover
-        showArrow={false}
-        overlayClassName={styles.popover}
-        content={
-          <BlockPicker
-            color={color}
-            onChangeComplete={(newColor) => {
-              if (onChange) onChange(newColor.hex);
-            }}
-          />
-        }
-      >
-        <div className={styles.color} style={{ backgroundColor: color }} />
-      </Popover>
-    }
-  />
-);
+const ColorPicker: FC<ColorPickerProps> = ({ color, onChange }) => {
+  const { theme: currentTheme } = theme.useToken();
+  const isDark = currentTheme.id === Theme.Dark;
+
+  return (
+    <StyledInput
+      value={color}
+      onChange={(e) => {
+        if (onChange) onChange(e.target.value);
+      }}
+      placeholder="Например #345f42"
+      addonBefore={
+        <Popover
+          showArrow={false}
+          overlayClassName={styles.popover}
+          content={
+            <BlockPicker
+              color={color}
+              onChangeComplete={(newColor) => {
+                if (onChange) onChange(newColor.hex);
+              }}
+              className={classNames(styles.color_picker, {
+                [styles.color_picker_dark]: isDark,
+              })}
+            />
+          }
+        >
+          <div className={styles.color} style={{ backgroundColor: color }} />
+        </Popover>
+      }
+    />
+  );
+};
 
 ColorPicker.defaultProps = {
   color: undefined,
