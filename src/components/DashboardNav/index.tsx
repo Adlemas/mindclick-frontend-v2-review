f013235a6877524b67ec906426a76d8e2087feb0
Adlemas <qwer.009771@gmail.com>
2023-02-05 13:59:08 +0100

    feat: dynamic page title in dashboard nav

--- src/components/DashboardNav/index.tsx
+++ src/components/DashboardNav/index.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import { Layout, theme } from "antd";
 
+import Head from "next/head";
 import styles from "./styles.module.scss";
 import Theme from "@/types/theme";
 import Profile from "@/components/DashboardNav/Profile";
@@ -17,19 +18,25 @@ const DashboardNav: FC<IDashboardNavProps> = ({ children, title }) => {
   const { colorBgContainer } = token;
 
   return (
-    <Header
-      className={styles.header}
-      style={{
-        background: currentTheme.id === Theme.Dark ? colorBgContainer : "#fff",
-        borderColor: currentTheme.id === Theme.Light ? "#e8e8e8" : "#252525",
-      }}
-    >
-      <div>
-        <Profile />
-      </div>
-      <div>{title}</div>
-      <div>{children}</div>
-    </Header>
+    <>
+      <Head>
+        <title>MindClick - {title}</title>
+      </Head>
+      <Header
+        className={styles.header}
+        style={{
+          background:
+            currentTheme.id === Theme.Dark ? colorBgContainer : "#fff",
+          borderColor: currentTheme.id === Theme.Light ? "#e8e8e8" : "#252525",
+        }}
+      >
+        <div>
+          <Profile />
+        </div>
+        <div>{title}</div>
+        <div>{children}</div>
+      </Header>{" "}
+    </>
   );
 };
 
