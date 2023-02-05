81ec290703bcb1e8bce6563be487408a6950c5f7
Adlemas <qwer.009771@gmail.com>
2023-02-05 14:31:13 +0100

    feat: bind update member action for table's columns

--- src/components/Members/MembersTable/index.tsx
+++ src/components/Members/MembersTable/index.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { Table, TablePaginationConfig, Tag } from "antd";
 import { ColumnsType } from "antd/lib/table";
 import { isEqual } from "lodash";
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { RiEditFill } from "react-icons/ri";
 import moment from "moment";
 import { IUser } from "@/types/entity";
@@ -10,66 +10,18 @@ import { IUser } from "@/types/entity";
 import styles from "./styles.module.scss";
 import getFullName from "@/utils/getFullName";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { getMembersAction, LOAD_MEMBERS_SIZE } from "@/redux/slices/members";
+import {
+  getMembersAction,
+  LOAD_MEMBERS_SIZE,
+  setMember,
+} from "@/redux/slices/members";
 import Button from "@/components/UI/Button";
-
-const columns: ColumnsType<IUser> = [
-  {
-    title: "Имя",
-    dataIndex: "firstName",
-    key: "firstName",
-    render: (_, user) => getFullName(user),
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Очки",
-    dataIndex: "points",
-    key: "points",
-  },
-  {
-    title: "Кубков",
-    dataIndex: "rate",
-    key: "rate",
-  },
-  {
-    title: "Номер телефона",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Группа",
-    dataIndex: ["group", "name"],
-    key: "group.name",
-    // if group is null, then it will be "-"
-    render: (name: string, { group }) => (
-      <Tag color={group?.color ?? undefined} key={name}>
-        {name ?? "-"}
-      </Tag>
-    ),
-  },
-  {
-    title: "Создана",
-    dataIndex: "createdAt",
-    key: "createdAt",
-    render: (createdAt: string) => moment(createdAt).format("DD.MM.YYYY HH:mm"),
-  },
-  {
-    title: "Действия",
-    dataIndex: "actions",
-    key: "actions",
-    render: () => (
-      <Button icon={<RiEditFill />} type="outline" secondary>
-        Редактировать
-      </Button>
-    ),
-  },
-];
+import Panel from "@/components/UI/Panel";
+import AddMemberForm from "@/forms/AddMemberForm";
 
 const MembersTable: FC = () => {
+  const [updateMemberPanel, setUpdateMemberPanel] = useState<boolean>();
+
   const { loading, records, page, totalCount } = useAppSelector(
     (state) => state.members,
     isEqual
@@ -93,6 +45,83 @@ const MembersTable: FC = () => {
     [dispatch, page, totalCount]
   );
 
+  const handleUpdateMember = useCallback(
+    (member: IUser) => {
+      setUpdateMemberPanel(true);
+      dispatch(setMember(member));
+    },
+    [dispatch]
+  );
+
+  const handleUpdateMemberCancel = () => {
+    setUpdateMemberPanel(false);
+  };
+
+  const columns: ColumnsType<IUser> = useMemo(
+    () => [
+      {
+        title: "Имя",
+        dataIndex: "firstName",
+        key: "firstName",
+        render: (_, user) => getFullName(user),
+      },
+      {
+        title: "Email",
+        dataIndex: "email",
+        key: "email",
+      },
+      {
+        title: "Очки",
+        dataIndex: "points",
+        key: "points",
+      },
+      {
+        title: "Кубков",
+        dataIndex: "rate",
+        key: "rate",
+      },
+      {
+        title: "Номер телефона",
+        dataIndex: "phone",
+        key: "phone",
+      },
+      {
+        title: "Группа",
+        dataIndex: ["group", "name"],
+        key: "group.name",
+        // if group is null, then it will be "-"
+        render: (name: string, { group }) => (
+          <Tag color={group?.color ?? undefined} key={name}>
+            {name ?? "-"}
+          </Tag>
+        ),
+      },
+      {
+        title: "Создана",
+        dataIndex: "createdAt",
+        key: "createdAt",
+        render: (createdAt: string) =>
+          moment(createdAt).format("DD.MM.YYYY HH:mm"),
+      },
+      {
+        title: "Действия",
+        dataIndex: "actions",
+        key: "actions",
+        render: (_, member) => (
+          <Button
+            icon={<RiEditFill />}
+            type="outline"
+            onClick={() => handleUpdateMember(member)}
+            secondary
+          >
+            Редактировать
+          </Button>
+        ),
+      },
+    ],
+    [handleUpdateMember]
+  );
+
   useEffect(() => {
     dispatch(
       getMembersAction({
@@ -104,13 +133,23 @@ const MembersTable: FC = () => {
   }, []);
 
   return (
-    <Table
-      className={styles.table}
-      columns={columns}
-      loading={loading}
-      dataSource={records}
-      pagination={pagination}
-    />
+    <>
+      <Table
+        className={styles.table}
+        columns={columns}
+        loading={loading}
+        dataSource={records}
+        pagination={pagination}
+      />
+      <Panel
+        title="Редактирование участника"
+        open={updateMemberPanel}
+        width="25rem"
+        onClose={handleUpdateMemberCancel}
+      >
+        <AddMemberForm />
+      </Panel>
+    </>
   );
 };
 
